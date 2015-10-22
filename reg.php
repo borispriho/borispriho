50160a5805cdda4203b9b01ce219e90a339c0c91
borispriho <boris.priho@gmail.com>
2015-10-22 15:34:32 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -2,21 +2,19 @@
     <head>
     <title>Регистрация</title>
     </head>
-    <body background="1.jpg">
+    <body>
     <h2>Регистрация</h2>
     <form action="save_user.php" method="post">
     <!--**** save_user.php - это адрес обработчика.  То есть, после нажатия на кнопку "Зарегистрироваться", данные из полей  отправятся на страничку save_user.php методом "post" ***** -->
-<p><label>Фамилия<br></label</p>
-<input name="familija" type="text" size="15" maxlength="15"></p>
-<p><label>Имя<br></label</p>
-<input name="imja" type="text" size="15" maxlength="15"></p>
-<p><label>Отчество<br></label</p>
-<input name="otchestvo" type="text" size="15" maxlength="15"></p>
-<p><label>Ваш логин:<br></label>
-<input name="login" type="text" size="15" maxlength="15"></p>
+<p>
+    <label>Ваш логин:<br></label>
+    <input name="login" type="text" size="15" maxlength="15">
+    </p>
 <!--**** В текстовое поле (name="login" type="text") пользователь вводит свой логин ***** -->
-<p><label>Ваш пароль:<br></label>
-<input name="password" type="password" size="15" maxlength="15"></p>
+<p>
+    <label>Ваш пароль:<br></label>
+    <input name="password" type="password" size="15" maxlength="15">
+    </p>
 <!--**** В поле для паролей (name="password" type="password") пользователь вводит свой пароль ***** -->
 <p>
     <input type="submit" name="submit" value="Зарегистрироваться">
