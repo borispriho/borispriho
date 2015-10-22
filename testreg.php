0a8b4edd1c1ec6abd157cb06b1f1c3eace8630d5
borispriho <boris.priho@gmail.com>
2015-10-22 15:35:16 +0300

    Update testreg.php

--- testreg.php
+++ testreg.php
@@ -1,49 +1,42 @@
-<body background="1.jpg">
-<?
+<?php
+    session_start();//  вся процедура работает на сессиях. Именно в ней хранятся данные  пользователя, пока он находится на сайте. Очень важно запустить их в  самом начале странички!!!
 if (isset($_POST['login'])) { $login = $_POST['login']; if ($login == '') { unset($login);} } //заносим введенный пользователем логин в переменную $login, если он пустой, то уничтожаем переменную
-if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
-//заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
-if (isset($_POST['familija'])) { $familija=$_POST['familija']; if ($familija =='') { unset($familija);} }
-if (isset($_POST['imja'])) { $imja=$_POST['imja']; if ($imja =='') { unset($imja);} }
-if (isset($_POST['otchestvo'])) { $imja=$_POST['otchestvo']; if ($otchestvo =='') { unset($otchestvo);} }
-if (empty($login) or empty($password) or empty($familija) or empty($imja) or empty($otchestvo)) 
-//если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
+    if (isset($_POST['password'])) { $password=$_POST['password']; if ($password =='') { unset($password);} }
+    //заносим введенный пользователем пароль в переменную $password, если он пустой, то уничтожаем переменную
+if (empty($login) or empty($password)) //если пользователь не ввел логин или пароль, то выдаем ошибку и останавливаем скрипт
     {
     exit ("Вы ввели не всю информацию, вернитесь назад и заполните все поля!");
     }
     //если логин и пароль введены,то обрабатываем их, чтобы теги и скрипты не работали, мало ли что люди могут ввести
     $login = stripslashes($login);
     $login = htmlspecialchars($login);
-    $password = stripslashes($password);
+$password = stripslashes($password);
     $password = htmlspecialchars($password);
-    $familija = stripslashes($familija);
-    $familija = htmlspecialchars($familija);
-    $imja = stripslashes($imja);
-    $imja = htmlspecialchars($imja);
-    $otchestvo = stripslashes($otchestvo);
-    $otchestvo = htmlspecialchars($otchestvo);
- //удаляем лишние пробелы
+//удаляем лишние пробелы
     $login = trim($login);
     $password = trim($password);
-    $familija = trim($familija);
-    $imja = trim($imja);
-    $otchestvo = trim($otchestvo);
- // подключаемся к базе
+// подключаемся к базе
     include ("bd.php");// файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто измените путь
- // проверка на существование пользователя с таким же логином
-    $result = mysql_query("SELECT id FROM users WHERE login='$login'",$db);
+ 
+$result = mysql_query("SELECT * FROM users WHERE login='$login'",$db); //извлекаем из базы все данные о пользователе с введенным логином
     $myrow = mysql_fetch_array($result);
-    if (!empty($myrow['id'])) {
-    exit ("Извините, введённый вами логин уже зарегистрирован. Введите другой логин.");
-    }
- // если такого нет, то сохраняем данные
-    $result2 = mysql_query ("INSERT INTO users (login,password,imja,familija,otchestvo) VALUES('$login','$password','$imja','$familija','otchestvo')");
-    // Проверяем, есть ли ошибки
-    if ($result2=='TRUE')
+    if (empty($myrow['password']))
     {
-    echo "Вы успешно зарегистрированы! Теперь вы можете зайти на сайт. <a href='index.php'>Главная страница</a>";
+    //если пользователя с введенным логином не существует
+    exit ("Извините, введённый вами login или пароль неверный.");
+    }
+    else {
+    //если существует, то сверяем пароли
+    if ($myrow['password']==$password) {
+    //если пароли совпадают, то запускаем пользователю сессию! Можете его поздравить, он вошел!
+    $_SESSION['login']=$myrow['login'];
+    $_SESSION['id']=$myrow['id'];//эти данные очень часто используются, вот их и будет "носить с собой" вошедший пользователь
+    echo "Вы успешно вошли на сайт! <a href='index.php'>Главная страница</a>";
     }
  else {
-    echo "Ошибка! Вы не зарегистрированы.";
+    //если пароли не сошлись
+ 
+    exit ("Извините, введённый вами login или пароль неверный.");
+    }
     }
     ?>
